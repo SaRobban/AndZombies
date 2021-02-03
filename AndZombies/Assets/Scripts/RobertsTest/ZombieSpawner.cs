7f0e45cbf8b873027793047bc885708da776361d
Hawxix <74661923+Hawxix@users.noreply.github.com>
2021-02-03 11:58:45 +0100

    Revert "Merge branch 'main' of https://github.com/SaRobban/AndZombies into main"
    This reverts commit b6b8f636dad7b6418e007a61ff2f2f0e594989b6, reversing
    changes made to 402fa763158bf065a5c0b40b0da8322c4c74240b.

--- AndZombies/Assets/Scripts/RobertsTest/ZombieSpawner.cs
+++ AndZombies/Assets/Scripts/RobertsTest/ZombieSpawner.cs
@@ -1,14 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+
 public class ZombieSpawner : MonoBehaviour
 {
     public int maxZombies = 10;
-    public int zombieNumber;
     public GameObject zombieClone;
-
-    public TextMeshProUGUI uiText;
     // Start is called before the first frame update
     private void Start()
     {
@@ -17,20 +14,20 @@ public class ZombieSpawner : MonoBehaviour
 
     public void SpawnZombie()
     {
-        if (zombieNumber<maxZombies+1)
+        if (maxZombies > 0)
         {
             GameObject clone = Instantiate(zombieClone, transform.position, transform.rotation);
             clone.GetComponent<ZombieMovement>().spawner = this;
-            zombieNumber++;
         }
         else
         {
             print("GameOver");
         }
-
-        UpdateUI();
     }
-   void UpdateUI() {
-        uiText.text = "Zombie : " + zombieNumber + " / " + maxZombies;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
