dd36014e018765cf321f1925abcd36e71b6b3ee3
Robert Sandh <sarobban@gmail.com>
2021-02-04 12:06:28 +0100

    Update ZombieAnimationControll.cs

--- AndZombies/Assets/Scripts/RobertsTest/ZombieAnimationControll.cs
+++ AndZombies/Assets/Scripts/RobertsTest/ZombieAnimationControll.cs
@@ -97,7 +97,6 @@ public class ZombieAnimationControll : MonoBehaviour
 
     void setAnimation()
     {
-        
         if(oldState != zm.zombieState)
         {
             oldState = zm.zombieState;
